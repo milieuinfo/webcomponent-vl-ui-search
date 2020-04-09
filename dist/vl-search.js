57963b9ca70a52d39fbc6cc420fdf8ddc36fa46d
Tom Coemans <tom.coemans@gmail.com>
2020-04-09 13:42:42 +0200

    fixed failing Firefox e2e test

--- dist/vl-search.js
+++ dist/vl-search.js
@@ -1,6 +1,12 @@
 import { VlElement, define } from '/node_modules/vl-ui-core/dist/vl-core.js';
-import { VlButton } from '/node_modules/vl-ui-button/dist/vl-button.js';
-import { VlInputField } from '/node_modules/vl-ui-input-field/dist/vl-input-field.js';
+import '/node_modules/vl-ui-icon/dist/vl-icon.js';
+import '/node_modules/vl-ui-button/dist/vl-button.js';
+import '/node_modules/vl-ui-input-field/dist/vl-input-field.js';
+
+/**
+ * Search change event analoog aan native change event.
+ * @event VlSearch#change
+ */
 
 /**
  * VlSearch
@@ -9,12 +15,11 @@ import { VlInputField } from '/node_modules/vl-ui-input-field/dist/vl-input-fiel
  * 
  * @extends VlElement
  *
- * @event submit - Zoeken werd getriggerd. Het event bevat de zoekterm als detail.
- *
- * @property {string} data-vl-label - Attribuut wordt gebruikt als label voor zoekcriteria. Standaard 'Zoekterm'.
- * @property {boolean} block - Attribuut duidt aan dat een breed zoekveld met knop wordt getoond. (Dit is de standaardweergave.)
- * @property {boolean} inline - Attribuut duidt aan dat een smal zoekveld met kleine knop wordt gebruikt.
- * @property {boolean} alt - Attribuut bepaalt of de alternatieve weergave (witte achtergrond) wordt gebruikt. Niet van toepassing bij inline.
+ * @property {string} ['Zoekterm'] data-vl-label - Attribuut wordt gebruikt als label voor zoekcriteria.
+ * @property {string} ['Zoeken'] data-vl-submit-label - Attribuut wordt gebruikt als label voor de submit knop.
+ * @property {boolean} data-vl-block - Attribuut duidt aan dat een breed zoekveld met knop wordt getoond. Dit is de standaardweergave.
+ * @property {boolean} data-vl-inline - Attribuut duidt aan dat een smal zoekveld met kleine knop wordt gebruikt.
+ * @property {boolean} data-vl-alt - Attribuut bepaalt of de alternatieve weergave (witte achtergrond) wordt gebruikt. Alleen relevant in combinatie met data-vl-block.
  * 
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-search/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-search/issues|Issues}
@@ -22,41 +27,50 @@ import { VlInputField } from '/node_modules/vl-ui-input-field/dist/vl-input-fiel
  * 
  */
 export class VlSearch extends VlElement(HTMLElement) {
-  
-    constructor() {
-        super(`
-                <style>
-                    @import '/node_modules/vl-ui-search/dist/style.css';
-                    @import '/node_modules/vl-ui-button/dist/style.css';
-                    @import '/node_modules/vl-ui-input-field/dist/style.css';              
-                </style>
-                <div id="search" class="vl-search">
-                    <input is="vl-input-field" 
-                           class="vl-search__input" 
-                           type="search"
-                           id="search-input" 
-                           value="" 
-                           title="Zoekterm" 
-                           required />
-                </div>
-            `);
-        this.__registreerZoekBijEnter();
-    }
-
-    connectedCallback() {
-        if (!this.hasAttribute('inline') && !this.hasAttribute('block')) {
-            // default to block if none set
-            this.__toggleInlineBlock(false);
-        }
-        this.__zoekknopElement.onclick = this._submit.bind(this);
-    }
-
     static get _observedAttributes() {
-        return ['data-vl-label'];
+        return ['label', 'submit-label'];
     }
 
     static get _observedChildClassAttributes() {
-        return ['block', 'inline', 'alt'];
+        return ['inline', 'block', 'alt'];
+    }
+
+    constructor() {
+        super(`
+            <style>
+                @import '/node_modules/vl-ui-search/dist/style.css';
+                @import '/node_modules/vl-ui-icon/dist/style.css';
+                @import '/node_modules/vl-ui-button/dist/style.css';
+                @import '/node_modules/vl-ui-input-field/dist/style.css';              
+            </style>
+            <div class="vl-search">
+                <input is="vl-input-field" class="vl-search__input" type="search" id="search-input" value="" title="Zoekterm" required />
+            </div>
+        `);
+    }
+
+    connectedCallback() {
+        if (!this._isInline && !this._isBlock) {
+            this.setAttribute('data-vl-block', ''); // default to block if none set
+        }
+        this.__setupChangeEventTriggers();
+    }
+
+    /**
+     * Geeft de zoekterm.
+     * 
+     * @return {String}
+     */
+    get value() {
+        return this.__inputElement.value;
+    }
+
+    get _isInline() {
+        return this.hasAttribute('data-vl-inline');
+    }
+
+    get _isBlock() {
+        return this.hasAttribute('data-vl-block');
     }
 
     get _classPrefix() {
@@ -67,119 +81,89 @@ export class VlSearch extends VlElement(HTMLElement) {
         return this._element.querySelector('#search-label');
     }
 
-    get __zoekknopElement() {
+    get __buttonElement() {
         return this._element.querySelector('#search-button');
     }
 
-    get __zoekveldElement() {
+    get __inputElement() {
         return this._element.querySelector('#search-input');
     }
 
-    _blockChangedCallback(oldValue, newValue) {
-        this.__toggleInlineBlock(newValue == undefined);
-        this.__zetLabelEnKnop();
-    }
-
     _inlineChangedCallback(oldValue, newValue) {
-        this.__toggleInlineBlock(newValue != undefined);
-        this.__zetLabelEnKnop();
+        this.toggleAttribute('data-vl-block', newValue == undefined);
+        this.__render();
     }
 
-    __toggleInlineBlock(inline) {
-        if (inline) {
-            this.removeAttribute('block');
-            if (!this.hasAttribute('inline')) {
-                this.setAttribute('inline', '');
-            }
-        } else {
-            this.removeAttribute('inline');
-            if (!this.hasAttribute('block')) {
-                this.setAttribute('block', '');
-            }
-        }
+    _blockChangedCallback(oldValue, newValue) {
+        this.toggleAttribute('data-vl-inline', newValue == undefined);
+        this.__render();
     }
 
-    _data_vl_labelChangedCallback() {
-        if (this.__isInlineLabel) {
-            this.__labelElement.querySelector('span').innerText = this.__labelTekst();
-        } else {
-            this.__labelElement.innerText = this.__labelTekst();
-        }
+    _labelChangedCallback() {
+        this.__renderLabel();
     }
 
-    get zoekterm() {
-        return this.__zoekveldElement.value;
+    _submit_labelChangedCallback() {
+        this.__renderButton();
+    }
+
+    __setupChangeEventTriggers() {
+        this.__inputElement.addEventListener('change', (event) => {
+            event.stopPropagation();
+            this._submit();
+        });
     }
 
     _submit() {
-        const event = new CustomEvent("submit", {
-            detail: {
-                zoekterm: this.zoekterm
-            }
-        });
-        this.dispatchEvent(event);
+        this.dispatchEvent(new Event('change'));
     }
 
-    get __isInlineLabel() {
-        return [...this.attributes].map(att => att.name).includes('inline');
+    __render() {
+        this.__renderLabel();
+        this.__renderButton();
     }
 
-    __registreerZoekBijEnter() {
-        this.__zoekveldElement.addEventListener("keydown", event => {
-            if (event.key === 'Enter') {
-                this._submit();
-            }
-        });
-    }
-
-    __zetLabelEnKnop() {
+    __renderLabel() {
         if (this.__labelElement) {
-            delete this.__labelElement;
+            this.__labelElement.remove();
         }
-        if (this.__isInlineLabel) {
-            this._element.prepend(this.__inlineLabelTemplate());
-            this._element.appendChild(this.__zoekKnopTemplate(true))
-        } else {
-            this._element.prepend(this.__blockLabelTemplate());
-            this._element.appendChild(this.__zoekKnopTemplate(false))
-        }
+        this._element.prepend(this.__getLabelTemplate());
     }
 
-    __inlineLabelTemplate() {
+    __renderButton() {
+        if (this.__buttonElement) {
+            this.__buttonElement.remove();
+        }
+        this._element.append(this.__getButtonTemplate());
+    }
+
+    __iconTemplate() {
+        return `<span is="vl-icon" data-vl-icon="magnifier" aria-hidden="true"></span>`;
+    }
+
+    __getLabelTemplate() {
+        const text = this.dataset.vlLabel || 'Zoekterm';
+        const content = this._isInline ? `<span class="vl-u-visually-hidden">${text}</span> ${this.__iconTemplate()}` : text;
         return this._template(`
             <label id="search-label" class="vl-search__label" for="search-input">
-                <span class="vl-u-visually-hidden">${this.__labelTekst()}</span>
-                <i class="vl-vi vl-vi-magnifier" aria-hidden="true"></i>
+                <slot name="label">
+                    ${content}
+                </slot>
             </label>
         `);
     }
 
-    __blockLabelTemplate() {
+    __getButtonTemplate() {
+        const content = this._isInline ? this.__iconTemplate() : ``;
         return this._template(`
-            <label id="search-label" 
-                   class="vl-search__label" 
-                   for="search-input">${this.__labelTekst()}</label>
-        `);
-    }
-
-    __zoekKnopTemplate(inline) {
-        return this._template(`
-            <button is="vl-button"
-                    id="search-button"
-                    class="vl-search__submit"
-                    type="submit">
-                ${inline 
-                    ? '<i class="vl-vi vl-vi-magnifier" aria-hidden="true"></i>' 
-                    : ''}         
-                Zoeken
+            <button is="vl-button" id="search-button" class="vl-search__submit" type="submit">
+                ${content}
+                <slot name="submit-label">
+                    ${this.dataset.vlSubmitLabel || 'Zoeken'}
+                </slot>
             </button>
         `);
     }
-
-    __labelTekst() {
-        return this.dataset.vlLabel || 'Zoekterm';
-    }
-
 }
 
 define('vl-search', VlSearch);
