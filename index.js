90f462c07b653dbfcdca7fd8ed0d6e9e749d7316
coemans <tom.coemans@gmail.com>
2020-06-03 16:42:22 +0200

    Introductie vl-ui-demo en ESlint (#14)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports = { 
-    Test: {
-        VlSearch: require('./test/e2e/components/vl-search.js').VlSearch
-    }
-}
+module.exports = {
+  Test: {
+    VlSearch: require('./test/e2e/components/vl-search.js').VlSearch,
+  },
+};
--- test/e2e/pages/vl-search.page.js
+++ test/e2e/pages/vl-search.page.js
@@ -1,41 +1,41 @@
 const VlSearch = require('../components/vl-search');
-const { Page, Config } = require('vl-ui-core').Test;
-const { By } = require('vl-ui-core').Test.Setup;
+const {Page, Config} = require('vl-ui-core').Test;
+const {By} = require('vl-ui-core').Test.Setup;
 
 class VlSearchPage extends Page {
-    async getInlineSearch() {
-        return this._getSearch('#search-inline');
-    }
+  async getInlineSearch() {
+    return this._getSearch('#search-inline');
+  }
 
-    async getBlockSearch() {
-        return this._getSearch('#search-block');
-    }
+  async getBlockSearch() {
+    return this._getSearch('#search-block');
+  }
 
-    async getAltSearch() {
-        return this._getSearch('#search-block-alt');
-    }
+  async getAltSearch() {
+    return this._getSearch('#search-block-alt');
+  }
 
-    async getLabelSearch() {
-        return this._getSearch('#search-block-label');
-    }
+  async getLabelSearch() {
+    return this._getSearch('#search-block-label');
+  }
 
-    async getSlotLabelSearch() {
-        return this._getSearch('#search-block-slot-label');
-    }
+  async getSlotLabelSearch() {
+    return this._getSearch('#search-block-slot-label');
+  }
 
-    async getSearchValues() {
-        const elements = await this.driver.findElements(By.css('[name=zoekterm]'));
-        const values = await Promise.all(elements.map(element => element.getText()));
-        return values.map(value => value.replace('Zoekterm: ', ''));
-    }
+  async getSearchValues() {
+    const elements = await this.driver.findElements(By.css('[name=zoekterm]'));
+    const values = await Promise.all(elements.map((element) => element.getText()));
+    return values.map((value) => value.replace('Zoekterm: ', ''));
+  }
 
-    async load() {
-        await super.load(Config.baseUrl + '/demo/vl-search.html');
-    }
+  async load() {
+    await super.load(Config.baseUrl + '/demo/vl-search.html');
+  }
 
-    async _getSearch(selector) {
-        return new VlSearch(this.driver, selector);
-    }
+  async _getSearch(selector) {
+    return new VlSearch(this.driver, selector);
+  }
 }
 
 module.exports = VlSearchPage;
