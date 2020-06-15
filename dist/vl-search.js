c65c0b85ecea60e373bbc1fc9c21fbd42a1677e9
Tom Coemans <tom.coemans@gmail.com>
2020-06-15 11:14:01 +0200

    dist build

--- dist/vl-search.js
+++ dist/vl-search.js
@@ -1,4 +1,5 @@
 import {vlElement, define} from '/node_modules/vl-ui-core/dist/vl-core.js';
+import {VlSelect} from '/node_modules/vl-ui-select/dist/vl-select.js';
 import '/node_modules/vl-ui-icon/dist/vl-icon.js';
 import '/node_modules/vl-ui-button/dist/vl-button.js';
 import '/node_modules/vl-ui-input-field/dist/vl-input-field.js';
@@ -25,162 +26,166 @@ import '/node_modules/vl-ui-input-field/dist/vl-input-field.js';
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-search/issues|Issues}
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-search.html|Demo}
  */
-export class VlSearch extends VlElement(HTMLElement) {
-    static get _observedAttributes() {
-        return ['label', 'submit-label'];
-    }
+export class VlSearch extends vlElement(HTMLElement) {
+  static get _observedAttributes() {
+    return ['label', 'submit-label'];
+  }
 
-    static get _observedChildClassAttributes() {
-        return ['inline', 'block', 'alt'];
-    }
+  static get _observedChildClassAttributes() {
+    return ['inline', 'block', 'alt'];
+  }
 
-    constructor() {
-        super(`
-            <style>
-                @import '/node_modules/vl-ui-search/dist/style.css';
-                @import '/node_modules/vl-ui-icon/dist/style.css';
-                @import '/node_modules/vl-ui-button/dist/style.css';
-                @import '/node_modules/vl-ui-input-field/dist/style.css';              
-            </style>
-            <div class="vl-search">
-                <slot name="input"></slot>
-                <input is="vl-input-field" class="vl-search__input" type="search" id="search-input" value="" title="Zoekterm" required />
-            </div>
-        `);
-    }
+  constructor() {
+    super(`
+      <style>
+        @import '/node_modules/vl-ui-search/dist/style.css';
+        @import '/node_modules/vl-ui-icon/dist/style.css';
+        @import '/node_modules/vl-ui-button/dist/style.css';
+        @import '/node_modules/vl-ui-input-field/dist/style.css';
+      </style>
+      <div class="vl-search">
+        <slot name="input"></slot>
+        <input is="vl-input-field" class="vl-search__input" type="search" id="search-input" value="" title="Zoekterm" required />
+      </div>
+    `);
+  }
 
-    connectedCallback() {
-        if (!this._isInline && !this._isBlock) {
-            this.setAttribute('data-vl-block', ''); // default to block if none set
-        }
-        this.__processInputSlot();
-        this.__setupChangeEventTriggers();
+  connectedCallback() {
+    if (!this._isInline && !this._isBlock) {
+      this.setAttribute('data-vl-block', ''); // default to block if none set
     }
+    this.__processInputSlot();
+    this.__setupChangeEventTriggers();
+  }
 
-    /**
-     * Geeft de zoekterm.
-     * 
-     * @return {String}
-     */
-    get value() {
-       return this.__inputElement.value;
+  /**
+   * Geeft de zoekterm.
+   *
+   * @return {String}
+   */
+  get value() {
+    return this.__inputElement.value;
+  }
+
+  get _isInline() {
+    return this.hasAttribute('data-vl-inline');
+  }
+
+  get _isBlock() {
+    return this.hasAttribute('data-vl-block');
+  }
+
+  get _classPrefix() {
+    return 'vl-search--';
+  }
+
+  get __labelElement() {
+    return this._element.querySelector('#search-label');
+  }
+
+  get __buttonElement() {
+    return this._element.querySelector('#search-button');
+  }
+
+  get __inputElement() {
+    return this._element.querySelector('#search-input');
+  }
+
+  get __inputSlotElement() {
+    return this._element.querySelector('slot[name="input"]');
+  }
+
+  _inlineChangedCallback(oldValue, newValue) {
+    this.toggleAttribute('data-vl-block', newValue == undefined);
+    this.__render();
+  }
+
+  _blockChangedCallback(oldValue, newValue) {
+    this.toggleAttribute('data-vl-inline', newValue == undefined);
+    this.__render();
+  }
+
+  _labelChangedCallback() {
+    this.__renderLabel();
+  }
+
+  _submitLabelChangedCallback() {
+    this.__renderButton();
+  }
+
+  __setupChangeEventTriggers() {
+    if (this.__inputElement) {
+      this.__inputElement.addEventListener('change', (event) => {
+        event.stopPropagation();
+        this._submit();
+      });
     }
+  }
 
-    get _isInline() {
-        return this.hasAttribute('data-vl-inline');
+  _submit() {
+    this.dispatchEvent(new Event('change'));
+  }
+
+  __render() {
+    this.__renderLabel();
+    this.__renderButton();
+  }
+
+  __renderLabel() {
+    if (this.__labelElement) {
+      this.__labelElement.remove();
     }
+    this._element.prepend(this.__getLabelTemplate());
+  }
 
-    get _isBlock() {
-        return this.hasAttribute('data-vl-block');
+  __renderButton() {
+    if (this.__buttonElement) {
+      this.__buttonElement.remove();
     }
+    this._element.append(this.__getButtonTemplate());
+  }
 
-    get _classPrefix() {
-        return 'vl-search--';
-    }
+  __iconTemplate() {
+    return `<span is="vl-icon" data-vl-icon="magnifier" aria-hidden="true"></span>`;
+  }
 
-    get __labelElement() {
-        return this._element.querySelector('#search-label');
-    }
-
-    get __buttonElement() {
-        return this._element.querySelector('#search-button');
-    }
-
-    get __inputElement() {
-        return this._element.querySelector('#search-input');
-    }
-
-    get __inputSlotElement() {
-        return this._element.querySelector('slot[name="input"]');
-    }
-
-    _inlineChangedCallback(oldValue, newValue) {
-        this.toggleAttribute('data-vl-block', newValue == undefined);
-        this.__render();
-    }
-
-    _blockChangedCallback(oldValue, newValue) {
-        this.toggleAttribute('data-vl-inline', newValue == undefined);
-        this.__render();
-    }
-
-    _labelChangedCallback() {
-        this.__renderLabel();
-    }
-
-    _submit_labelChangedCallback() {
-        this.__renderButton();
-    }
-
-    __setupChangeEventTriggers() {
-        if (this.__inputElement) {
-            this.__inputElement.addEventListener('change', (event) => {
-                event.stopPropagation();
-                this._submit();
-            });
-        }
-    }
-
-    _submit() {
-        this.dispatchEvent(new Event('change'));
-    }
-
-    __render() {
-        this.__renderLabel();
-        this.__renderButton();
-    }
-
-    __renderLabel() {
-        if (this.__labelElement) {
-            this.__labelElement.remove();
-        }
-        this._element.prepend(this.__getLabelTemplate());
-    }
-
-    __renderButton() {
-        if (this.__buttonElement) {
-            this.__buttonElement.remove();
-        }
-        this._element.append(this.__getButtonTemplate());
-    }
-
-    __iconTemplate() {
-        return `<span is="vl-icon" data-vl-icon="magnifier" aria-hidden="true"></span>`;
-    }
-
-    __getLabelTemplate() {
-        const text = this.dataset.vlLabel || 'Zoekterm';
-        const content = this._isInline ? `<span class="vl-u-visually-hidden">${text}</span> ${this.__iconTemplate()}` : text;
-        return this._template(`
-            <label id="search-label" class="vl-search__label" for="search-input">
-                <slot name="label">
-                    ${content}
-                </slot>
-            </label>
-        `);
+  __getLabelTemplate() {
+    const text = this.dataset.vlLabel || 'Zoekterm';
+    const content = this._isInline ? `<span class="vl-u-visually-hidden">${text}</span> ${this.__iconTemplate()}` : text;
+    return this._template(`
+      <label id="search-label" class="vl-search__label" for="search-input">
+        <slot name="label">
+          ${content}
+        </slot>
+      </label>
+    `);
   }
 
   __getButtonTemplate() {
     const content = this._isInline ? this.__iconTemplate() : ``;
     return this._template(`
-            <button is="vl-button" id="search-button" class="vl-search__submit" type="submit">
-                ${content}
-                <slot name="submit-label">
-                    ${this.dataset.vlSubmitLabel || 'Zoeken'}
-                </slot>
-            </button>
-        `);
-    }
+      <button is="vl-button" id="search-button" class="vl-search__submit" type="submit">
+        ${content}
+        <slot name="submit-label">
+          ${this.dataset.vlSubmitLabel || 'Zoeken'}
+        </slot>
+      </button>
+    `);
+  }
 
-    __processInputSlot() {
-        const slot = this.querySelector('[slot="input"]');
-        if (! slot) {
-            this._shadow.querySelector('slot[name="input"]').remove();
-        } else {
-            slot.classList.add('vl-search__input');
-            this.__inputElement.remove();
-        }
+  __processInputSlot() {
+    const slot = this.querySelector('[slot="input"]');
+    if (!slot) {
+      this._shadow.querySelector('slot[name="input"]').remove();
+    } else {
+      if (slot instanceof VlSelect) {
+        slot.addEventListener(VlSelect.readyEvent, (event) => {
+          event.target._wrapperElement.classList.add('vl-search__input');
+        });
+      }
+      this.__inputElement.remove();
     }
+  }
 }
 
 define('vl-search', VlSearch);
